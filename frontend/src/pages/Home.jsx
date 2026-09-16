da6b9d04f7445b3819a754bcf8e166ea9a383303
KhanhBaoLe <khanhlebao26@gmail.com>
2026-09-16 22:44:15 +0700

    add upcoming release component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import FeaturedGenres from "../components/FeaturedGenres";
 import HomeHero from "../components/HomeHero";
 import HomeMovies from "../components/HomeMovies";
 import TrendingPeople from "../components/TrendingPeople";
+import UpcomingReleases from "../components/UpcomingReleases";
 import WeeklyRanking from "../components/WeeklyRanking";
 
 export default function Home({ search, genre }) {
@@ -22,6 +23,8 @@ export default function Home({ search, genre }) {
             />
 
             <TrendingPeople />
+
+            <UpcomingReleases/>
         </>
     );
 }
