41b81dbd9936edf7680b7da70dd5ec40e58eaf2c
KhanhBaoLe <khanhlebao26@gmail.com>
2026-09-21 21:09:10 +0700

    fix(auth): fix typo and properly clear JWT cookie on logout

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -94,14 +94,13 @@ const login = async (req, res) => {
 };
 
 const logout = async (req, res) => {
-    res.cookie("jwt", "", {
+    res.clearCookie("jwt", {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "producttion",
+        secure: process.env.NODE_ENV === "production",
         sameSite:
-            process.env.NODE_ENV === "producttion"
+            process.env.NODE_ENV === "production"
                 ? "strict"
                 : "lax",
-        // expires: new Date(0)
     });
     res.status(200).json({
         status: "success",
